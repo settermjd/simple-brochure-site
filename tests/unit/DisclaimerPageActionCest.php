9a8823cfcbb90adf30acefac5e556a5bef4238fd
Matthew Setter <matthew@matthewsetter.com>
2016-10-28 11:00:08 +0200

    Create the core of the Disclaimer page
    This commit includes the page action class, along with a unit test to
    provide basic validation for it.

---
+++ tests/unit/DisclaimerPageActionCest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Action\DisclaimerPageAction;
+use Codeception\Util\Stub;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
+
+class DisclaimerPageActionCest
+{
+    private $router;
+    private $template;
+
+    public function _before(UnitTester $I)
+    {
+        $this->router = Stub::makeEmpty('\Zend\Expressive\Router\RouterInterface', []);
+        $this->template = Stub::makeEmpty('\Zend\Expressive\Template\TemplateRendererInterface', [
+            'render' => ''
+        ]);
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+        $page = new DisclaimerPageAction($this->router, $this->template);
+        $response = $page(new ServerRequest(['/disclaimer']), new Response(), function () {});
+
+        $I->assertTrue($response instanceof Response\HtmlResponse);
+    }
+}
