01552e27135899ab0bc624b6822f10e679beb32b
Matthew Setter <matthew@matthewsetter.com>
2016-10-28 16:53:47 +0200

    Refactor contact page assertions to a PageObject function
    This shows an example of using a PageObject method to provide for
    reusable test code.

--- tests/_support/Page/ContactPage.php
+++ tests/_support/Page/ContactPage.php
@@ -23,5 +23,28 @@ class ContactPage
         return static::$URL.$param;
     }
 
-
+    public function validateContactPage(\AcceptanceTester $I, \Page\ContactPage $contactPage)
+    {
+        $I->canSee(ucfirst($contactPage::$TITLE), '//h1');
+        $I->canSeeInTitle(ucfirst($contactPage::$TITLE));
+        $I->canSeeElement('//input', [
+            'name' => 'email',
+            'type' => 'email',
+            'class' => 'form-control',
+        ]);
+        $I->canSeeElement('//input', [
+            'name' => 'name',
+            'type' => 'text',
+            'class' => 'form-control',
+        ]);
+        $I->canSeeElement('//textarea', [
+            'name' => 'message',
+            'class' => 'form-control',
+        ]);
+        $I->canSeeElement('//input', [
+            'name' => 'submit',
+            'type' => 'submit',
+            'class' => 'btn btn-default btn-block',
+        ]);
+    }
 }
--- tests/acceptance/ContactCest.php
+++ tests/acceptance/ContactCest.php
@@ -3,35 +3,15 @@
 
 class ContactCest
 {
-    public function canViewTheContactPage(
+    public function canViewAndSubmitTheContactPage(
         AcceptanceTester $I,
         \Page\ContactPage $contactPage
     ) {
         $I->am('guest user');
         $I->wantTo('visit the contact page');
-        $I->lookForwardTo('check that everything is as it should be');
+        $I->lookForwardTo('check that everything is as it should be and that a value can be submitted');
         $I->amOnPage($contactPage::$URL);
         $I->canSeeResponseCodeIs(200);
-        $I->canSee(ucfirst($contactPage::$TITLE), '//h1');
-        $I->canSeeInTitle(ucfirst($contactPage::$TITLE));
-        $I->canSeeElement('//input', [
-            'name' => 'email',
-            'type' => 'email',
-            'class' => 'form-control',
-        ]);
-        $I->canSeeElement('//input', [
-            'name' => 'name',
-            'type' => 'text',
-            'class' => 'form-control',
-        ]);
-        $I->canSeeElement('//textarea', [
-            'name' => 'message',
-            'class' => 'form-control',
-        ]);
-        $I->canSeeElement('//input', [
-            'name' => 'submit',
-            'type' => 'submit',
-            'class' => 'btn btn-default btn-block',
-        ]);
+        $contactPage->validateContactPage($I, $contactPage);
     }
 }
