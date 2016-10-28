36f8f7d3f71b0776398b8cc2db4ad83288396d06
Matthew Setter <matthew@matthewsetter.com>
2016-10-28 10:37:54 +0200

    Convert Cept test to Cest format
    This isn't strictly required, but provides a good, if simple, example of
    both formats, without being overly sophisticated.

---
+++ tests/acceptance/HomeCest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class HomeCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function testHomePage(AcceptanceTester $I)
+    {
+        $I->am('guest user');
+        $I->wantTo('visit home page');
+        $I->lookForwardTo('check that everything is as it should be');
+        $I->amOnPage('/');
+        $I->canSeeResponseCodeIs(200);
+        $I->canSee('Welcome to the Simple Brochure Site');
+        $I->canSee('This is a wonderfully uninteresting application, designed to support the Codeception course for PluralSight.');
+    }
+}
