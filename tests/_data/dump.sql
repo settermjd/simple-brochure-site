377a6ae4bfdaae808f562cbe692476e9a9569a82
Matthew Setter <matthew@matthewsetter.com>
2016-10-31 13:38:43 +0100

    Configure Codeception's Db module to use a test database
    This also includes a custom truncate table method, which
    doesn't quite work yet.

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,16 @@
 /* Replace this file with actual dump of your database */
+PRAGMA foreign_keys = false;
+
+-- ----------------------------
+--  Table structure for "tblcontact"
+-- ----------------------------
+DROP TABLE IF EXISTS "tblcontact";
+DROP TABLE IF EXISTS "sqlite_sequence";
+CREATE TABLE "tblcontact" (
+  "id" integer PRIMARY KEY AUTOINCREMENT,
+  "name" text(100,0) NOT NULL,
+  "email" text(100,0) NOT NULL,
+  "message" text NOT NULL
+);
+
+PRAGMA foreign_keys = true;
--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -4,7 +4,13 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\Module\Db;
+
 class Unit extends \Codeception\Module
 {
-
+    function truncateDb($table) {
+        /** @var Db $dbh */
+        $dbh = $this->getModule('Db');
+        $dbh->driver->executeQuery('DELETE FROM tblcontact WHERE 1=1', []);
+    }
 }
